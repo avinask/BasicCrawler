f7ae9842539af2a53c0a99d4d7ff2126fafc09f9
Avinash <Avinash@Avinash-PC>
2016-01-01 13:45:42 +0530

    test

---
+++ SimpleCrawler/src/com/crawler/simple/CSVReader.java
@@ -0,0 +1,15 @@
+package com.crawler.simple;
+
+import java.io.BufferedReader;
+import java.io.File;
+
+public class CSVReader {
+
+	BufferedReader buffer;
+	protected String CSVInput()
+	{
+
+		return "";	
+
+	}
+}
